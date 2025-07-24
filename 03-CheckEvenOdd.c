6f84c2cd96f284c2a73cf59b84fd901ccc339e9b
Samir Das <samir.das1055@gmail.com>
2025-07-24 20:56:25 +0530

    3 & 4

---
+++ 03-CheckEvenOdd.c
@@ -0,0 +1,17 @@
+//3. Check whether a number is even or odd.
+#include<stdio.h>
+int main()
+{
+  int number;
+  printf("Enter an integer: ");
+  scanf("%d",&number);
+  if (number%2==0)
+  {
+    printf("%d is even.\n",number);
+  }
+  else
+  {
+    printf("%d is odd.\n",number);
+  }
+  return 0;
+}
